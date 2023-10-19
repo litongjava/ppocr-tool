fb50d1d5212f7f6acdf0f58766bd739dad2d99e9
litongjava <litongjava@qq.com>
2023-10-18 17:46:46 -1000

    增加main.py

---
+++ main-ocr-text.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import sys
+
+paddleocr_path = os.environ.get('PADDLEOCR_PATH')
+if paddleocr_path:
+    sys.path.insert(0, paddleocr_path)
+
+import paddle
+import paddleocr
+
+
+def version():
+    print(f"Python Version: {sys.version}")
+    print(f"Paddle Version: {paddle.__version__}")
+    print(f"PaddleOCR Version: {paddleocr.__version__}")
+
+
+def ocr(source, language):
+    # 检测+方向分类器+识别全流程
+    from paddleocr import PaddleOCR
+
+    ocr = PaddleOCR(use_angle_cls=True, lang=language)  # need to run only once to download and load model into memory
+
+    result = ocr.ocr(source, cls=True)
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            coordinates, content_info = line  # 解包line
+            text, confidence = content_info  # 解包content_info
+            print(text)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='PaddleOCR Utility')
+
+    # 添加参数
+    parser.add_argument('-v', '--version', action='store_true', help='输出版本')
+    parser.add_argument('-src', '--source', type=str, help='图像文件路径')
+    parser.add_argument('-lang', '--language', type=str, help='语言')
+
+    args = parser.parse_args()
+
+    # 根据参数执行相应的操作
+    if args.version:
+        return version()
+
+    if args.source:
+        ocr(args.source, args.language);
+
+
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-
+# ppocr-tool
 ## 准备环境
 ```
-conda create --name paddle_ocr_cpu_env python=3.8
+conda create --name paddle_ocr_cpu_env python=3.8 -y
 activate paddle_ocr_cpu_env 
 or
 conda activate paddle_ocr_cpu_env 
@@ -12,7 +12,7 @@ pip install "paddleocr>=2.0.1" -i https://mirror.baidu.com/pypi/simple
 ```
 ## 识别图片
 ```
--src E:\code\python\project-github\PaddleOCR\doc\imgs\11.jpg -lang ch
+python main-ocr-text.py -src E:\code\python\project-github\PaddleOCR\doc\imgs\11.jpg -lang ch
 ```
 
 识别PDF
@@ -31,16 +31,14 @@ pip install "paddleocr>=2.0.1" -i https://mirror.baidu.com/pypi/simple
 ## 模型 
 首次运行会自动下载模型
 中文模型
-
-https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_det_infer.tar
-https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_rec_infer.tar
-https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
+- https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_det_infer.tar
+- https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_rec_infer.tar
+- https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
 
 
 英文模型
-
-https://paddleocr.bj.bcebos.com/PP-OCRv4/english/en_PP-OCRv4_rec_infer.tar
-
+- https://paddleocr.bj.bcebos.com/PP-OCRv4/english/en_PP-OCRv4_rec_infer.tar
 
 模型保存位置
-/Users/ping/.paddleocr/whl
+- windows C:\Users\Administrator\.paddleocr\whl
+- macos /Users/ping/.paddleocr/whl
